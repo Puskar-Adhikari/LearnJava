3c665885e3565d461e5bf8785b91f293c1c29c21
puskar adhikari <beyondidsdet@gmail.com>
2023-07-13 21:32:28 +0545

    Learn Java with Puskar Adhikari

--- src/main/java/chapter3/LoanQualifier.java
+++ src/main/java/chapter3/LoanQualifier.java
@@ -1,4 +1,7 @@
 package chapter3;
+
+import java.util.Scanner;
+
 /*
 
 ------Nested If Statements-----------
@@ -12,5 +15,27 @@ To Qualify for a Loan, a person must make at least $30000 and
  */
 public class LoanQualifier {
 
+    public static void main(String[] args) {
 
+        int minimalSalary = 30000;
+        int minimalYears = 2;
+
+        System.out.println("Enter your current Salary ");
+        Scanner scanner = new Scanner(System.in);
+        int salary = scanner.nextInt();
+
+        System.out.println("Enter the number of years that you worked in this company");
+        double years = scanner.nextDouble();
+
+        if (salary >= minimalSalary){
+            if (years >= minimalSalary) {
+                System.out.println("Congratulation! you are eligible for the loan");
+            }else {
+                System.out.println("Sorry! you are not eligible for the loan, you must have work for the" + minimalYears + "years");
+
+            }
+
+        }else
+        System.out.println("Sorry! you must earn at least $ " +minimalSalary );
+    }
 }
