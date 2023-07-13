c470aa30f65b3f04e45a85bb7cc7bcfaaba1e7dc
puskar adhikari <beyondidsdet@gmail.com>
2023-07-13 20:48:05 +0545

    Learn Java with Puskar Adhikari

---
+++ src/main/java/chapter3/LoanQualifier.java
@@ -0,0 +1,16 @@
+package chapter3;
+/*
+
+------Nested If Statements-----------
+If a certain Situation occurs,
+Check for the next Situation
+
+
+To Qualify for a Loan, a person must make at least $30000 and
+        have been working at their current job for atleast 2 years
+
+ */
+public class LoanQualifier {
+
+
+}
